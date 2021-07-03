0bd97e5c2b2dfe180b229a87a4fef0835af2a985
chaudharykbc <kamalbchaudhary@yahoo.com>
2021-07-03 16:45:47 -0400

    Assignment 2 triangle solver analyze code file.

---
+++ Assignment02_SENG8040/Assignment02_SENG8040/TriangleSolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment02_SENG8040
+{
+    public class TriangleSolver
+    {
+         public string Analyze(int sidea, int sideb, int sidec)
+         {
+            if ((sidea + sideb) > sidec)
+            {
+                if ((sideb + sidec) > sidea)
+                {
+                    if ((sidea + sidec) > sideb)
+                    {
+                        Console.WriteLine("Triangle is Valid");
+                        if (sidea == sideb && sideb == sidec)
+                        {
+                            Console.WriteLine("This is an equilateral triangle.");
+                            return "This is an equilateral triangle.";
+                        }
+                        else if (sidea == sideb || sidea == sidec || sideb == sidec)
+                        {
+                            Console.WriteLine("This is an isosceles triangle.");
+                            return "This is an isosceles triangle.";
+                        }
+                        else
+                        {
+                            Console.WriteLine("This is a scalene triangle.");
+                            return "This is a scalene triangle.";
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Triangle is not valid.");
+                        return "Triangle is not valid.";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Triangle is not valid");
+                    return "Triangle is not valid.";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Triangle is not valid");
+                return "Triangle is not valid.";
+            }
+        }
+    }
+}
